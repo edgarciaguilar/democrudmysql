0796ad87906bf2357371f4bfa2f510f4acea11b6
edgarciaguilar <edgarciaguilar@gmail.com>
2023-07-26 16:16:47 -0600

    agregada la ruta de usuario

--- src/main/java/com/crud/democrudmysql/config/WebSecurityConfig.java
+++ src/main/java/com/crud/democrudmysql/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
             .authorizeRequests()
             .antMatchers("/admin/**")
             .hasAnyRole("ADMIN")
-            .antMatchers("/cursos/*")
+            .antMatchers("/cursos/*","/usuario/**")
             .authenticated()
             .anyRequest()
             .permitAll()
